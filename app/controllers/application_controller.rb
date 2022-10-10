4259a8f0f939845fb8be4dae1c2e74b8d9c1b4a9
Nick Story <storynickolasl@gmail.com>
2022-10-10 19:00:39 -0400

    commit tree e7b0c56546ad48a5373bbfbabe39bb0e7c0cc62b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    priced = BakedGood.all.order(price: :desc)
+
+    priced.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    # expensive = BakedGood.all.where("price = ?", BakedGood.all.maximum(:price))
+    expensive = BakedGood.all.order(price: :desc)[0]
+
+    expensive.to_json(only: [:name, :price])
+  end
 
 end
